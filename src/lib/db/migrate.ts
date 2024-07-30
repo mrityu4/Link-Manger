05fb60b4fb1eeb0ec3f8f8dc67557fd15a8b9287
Mritunjay Rai <mritunjayrai@MacBook-Air.local>
2024-07-30 22:31:37 +0530

    fix env var getter in migrations file

--- src/lib/db/migrate.ts
+++ src/lib/db/migrate.ts
@@ -1,4 +1,3 @@
-import { env } from "@/lib/env.mjs";
   
 import { createClient } from "@libsql/client";
 import { drizzle } from "drizzle-orm/libsql";
@@ -6,14 +5,14 @@ import { migrate } from "drizzle-orm/libsql/migrator";
 
 
 const runMigrate = async () => {
-  if (!env.DATABASE_URL) {
+  if (!process.env.DATABASE_URL) {
     throw new Error("DATABASE_URL is not defined");
   }
 
   
   const client = createClient({
-    url: env.DATABASE_URL,
-    authToken: env.DATABASE_AUTH_TOKEN,
+    url: process.env.DATABASE_URL,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
   });
   const db = drizzle(client);
 
